70a825609ed550e0cb9052211e11abefdc117286
RomanovRoma <romanian.reget@gmail.com>
2021-02-06 23:09:17 +0300

    add types of data

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 "use strict";
-console.log('Hello TypeScript');
-console.log(1 + true);
+// Типы данных
+// let a = 2
+// a = 'TypeScript'
+// a = 1 + 5
+// a = 1 + 'Roman'
+var a = 1;
+// let b = 'Hello'
+// b = 1
+// b = b + 'TypeScript'
+var b = 'Hello';
+// let c = true
+// c = 1
+// c = false
+var c = true;
+// let d = null
+var d = null;
+// d = 'Hello'
+// d = null
+// let e = undefined
+var e = undefined;
+// e = 1
+// e = undefined
+var f = {};
+// f = 'Hello'
+f = { text: 'Hello' };
+f.text;
+// let f: { text: string } = { text: 'Hello' }
+// f.text
+// const f: { text: string } = { text: "Hello" }
+// f = { text: 'Hello TypeScript' }
+// f.text = 'Hello'
+var g;
+g = 1;
+g = 'Hello';
+g = true;
+g = null;
+g = undefined;
+g = {};
+g = function () { };
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,2 +1,46 @@
-console.log('Hello TypeScript')
-console.log(1 + true)
+// Типы данных
+
+// let a = 2
+// a = 'TypeScript'
+// a = 1 + 5
+// a = 1 + 'Roman'
+let a: number = 1
+
+// let b = 'Hello'
+// b = 1
+// b = b + 'TypeScript'
+let b: string = 'Hello'
+
+// let c = true
+// c = 1
+// c = false
+let c: boolean = true
+
+// let d = null
+let d: null = null
+// d = 'Hello'
+// d = null
+
+// let e = undefined
+let e: undefined = undefined
+// e = 1
+// e = undefined
+
+let f: object = {}
+// f = 'Hello'
+f = { text: 'Hello' }
+f.text
+// let f: { text: string } = { text: 'Hello' }
+// f.text
+// const f: { text: string } = { text: "Hello" }
+// f = { text: 'Hello TypeScript' }
+// f.text = 'Hello'
+
+let g: any
+g = 1
+g = 'Hello'
+g = true
+g = null
+g = undefined
+g = {}
+g = function () {}
