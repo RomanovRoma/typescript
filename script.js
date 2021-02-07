8f6572f9d20d865d6a9ce269e48a5894a2e9f9d7
RomanovRoma <romanian.reget@gmail.com>
2021-02-07 09:29:52 +0300

    add union types

--- script.js
+++ script.js
@@ -41,43 +41,77 @@
 // function a(x, y) {
 //   return x + y
 // }
-function a(x, y) {
-    return x + y;
-}
-var b = function (x, y) { return x + y; };
-// Сигнатура функции:
-var c;
-// c = function (x: number, y: string): string {
+// function a(x: number, y: number): number {
+//   return x + y;
+// }
+// let b = (x: number, y: number): number => x + y
+// // Сигнатура функции:
+// let c: (x: number, y: number) => number
+// // c = function (x: number, y: string): string {
+// //   return x + y
+// // }
+// c = function (x: number, y; number): number {
 //   return x + y
 // }
-c = function (x, y, number) {
-    return x + y;
-};
-function d(x, y) {
-    console.log(x + y);
+// function d(x: number, y: number): void {
+//   console.log(x + y)
+// }
+// function e(x: string): never {
+//   throw new Error(x)
+// }
+// // Перезгрузка функции:
+// function f(x: any, y: any): any {
+//   if (typeof x === 'number') {
+//     return x + y
+//   } else {
+//     return `${x} ${y}`
+//   }
+// }
+// console.log(f(1, 2))
+// console.log(f(`Ответ`, 2))
+// function g(x: number, y: number): number
+// function g(x: string, y: number): string
+// function f(x: any, y: any): any {
+//   if (typeof x === 'number') {
+//     return x + y
+//   } else {
+//     return `${x} ${y}`
+//   }
+// }
+// console.log(g(1, 2))
+// console.log(g('Ответ', 3))
+var a = 1;
+var b = a;
+var c = 1;
+// let d: number = c
+var d = c;
+var e = c;
+var f = 'Hello';
+var g = f.length;
+var h = 1;
+h = 'Hi';
+var i = 1;
+i = 'Hello';
+var j = 1;
+j = 2;
+j = 3;
+var k = 'a';
+k = 'b';
+k = 'c';
+var l = true;
+var jkl = 'a';
+// jkl = 2
+jkl = true;
+jkl = false;
+var m = { a: '' };
+m = { b: '' };
+m = { a: '', b: '' };
+var n = { a: '', b: '' };
+var o = { a: '' };
+// o = { a: '', b: '' }
+// o = { a: '', b: '', c: '' }
+// o = {}
+if ('b' in o) {
+    console.log('TypeScript');
 }
-function e(x) {
-    throw new Error(x);
-}
-// Перезгрузка функции:
-function f(x, y) {
-    if (typeof x === 'number') {
-        return x + y;
-    }
-    else {
-        return x + " " + y;
-    }
-}
-console.log(f(1, 2));
-console.log(f("\u041E\u0442\u0432\u0435\u0442", 2));
-function f(x, y) {
-    if (typeof x === 'number') {
-        return x + y;
-    }
-    else {
-        return x + " " + y;
-    }
-}
-console.log(g(1, 2));
-console.log(g('Ответ', 3));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -49,48 +49,114 @@
 //   return x + y
 // }
 
-function a(x: number, y: number): number {
-  return x + y;
-}
+// function a(x: number, y: number): number {
+//   return x + y;
+// }
 
-let b = (x: number, y: number): number => x + y
+// let b = (x: number, y: number): number => x + y
 
-// Сигнатура функции:
-let c: (x: number, y: number) => number
-// c = function (x: number, y: string): string {
+// // Сигнатура функции:
+// let c: (x: number, y: number) => number
+// // c = function (x: number, y: string): string {
+// //   return x + y
+// // }
+// c = function (x: number, y; number): number {
 //   return x + y
 // }
-c = function (x: number, y; number): number {
-  return x + y
+
+// function d(x: number, y: number): void {
+//   console.log(x + y)
+// }
+
+// function e(x: string): never {
+//   throw new Error(x)
+// }
+
+// // Перезгрузка функции:
+// function f(x: any, y: any): any {
+//   if (typeof x === 'number') {
+//     return x + y
+//   } else {
+//     return `${x} ${y}`
+//   }
+// }
+// console.log(f(1, 2))
+// console.log(f(`Ответ`, 2))
+
+// function g(x: number, y: number): number
+// function g(x: string, y: number): string
+// function f(x: any, y: any): any {
+//   if (typeof x === 'number') {
+//     return x + y
+//   } else {
+//     return `${x} ${y}`
+//   }
+// }
+// console.log(g(1, 2))
+// console.log(g('Ответ', 3))
+
+let a: any = 1
+let b: number = a
+
+let c: unknown = 1
+// let d: number = c
+let d: number = <number>c
+let e: number = c as number
+
+let f: any = 'Hello'
+let g: number = (<string>f).length
+
+let h: number | string = 1
+h = 'Hi'
+// h = true
+
+type I = number | string
+let i: I = 1
+i = 'Hello'
+// i = true
+
+type J = 1 | 2 | 3
+let j: J = 1
+j = 2
+j = 3
+// j = 4
+
+type K = 'a' | 'b' | 'c'
+let k: K = 'a'
+k = 'b'
+k = 'c'
+// k = 'd'
+
+type L = true
+let l: L = true
+// l = false
+
+type JKL = 'a' | 1 | boolean
+let jkl: JKL = 'a'
+// jkl = 2
+jkl = true
+jkl = false
+
+type M = { a: string } | { b: string }
+let m: M = { a: ''}
+m = { b: ''}
+m = { a: '', b: ''}
+// m = { a: '', b: '', c: '' }
+// m = {}
+
+type N = { a: string } & { b: string }
+let n: N = { a: '', b: '' }
+// n = { a: '' }
+// n = { b: '' }
+// n = {}
+
+type O = { a: string, b?: string }
+let o: O = { a: '' }
+// o = { a: '', b: '' }
+// o = { a: '', b: '', c: '' }
+// o = {}
+
+if ('b' in o) {
+  console.log('TypeScript')
 }
 
-function d(x: number, y: number): void {
-  console.log(x + y)
-}
-
-function e(x: string): never {
-  throw new Error(x)
-}
-
-// Перезгрузка функции:
-function f(x: any, y: any): any {
-  if (typeof x === 'number') {
-    return x + y
-  } else {
-    return `${x} ${y}`
-  }
-}
-console.log(f(1, 2))
-console.log(f(`Ответ`, 2))
-
-function g(x: number, y: number): number
-function g(x: string, y: number): string
-function f(x: any, y: any): any {
-  if (typeof x === 'number') {
-    return x + y
-  } else {
-    return `${x} ${y}`
-  }
-}
-console.log(g(1, 2))
-console.log(g('Ответ', 3))
